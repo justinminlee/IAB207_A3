d3b2f712555c9cc1352fdd134b7b5d1e6ec7e93c
Justin Lee <smsc19970205@gmail.com>
2024-10-28 20:37:23 +1000

    Commented out models

--- website/models.py
+++ website/models.py
@@ -5,10 +5,10 @@ from flask_login import UserMixin
 #Here we need to create Tables for the database.
 #This will needs to be done before we run the application.
 
-class User(db.Model, UserMixin):
+#class User(db.Model, UserMixin):
 
-class Event(db.Model):
+#class Event(db.Model):
 
-class Comment(db.Model):
+#class Comment(db.Model):
 
-class Order(db.Model):
+#class Order(db.Model):
