ca2ff059adc4a321fbebc12c4bf934b68588c982
Justin Lee <smsc19970205@gmail.com>
2024-10-28 22:14:51 +1000

    Update form.py

--- website/forms.py
+++ website/forms.py
@@ -27,7 +27,7 @@ class RegisterForm(FlaskForm):
     
 #Create new event
 class EventForm(FlaskForm):
-    name = StringField('Name', validators=[InputRequired()])
+    name = StringField('Title of the Event', validators=[InputRequired()])
     description = TextAreaField('Description', 
                 validators=[InputRequired()])
     image = FileField('Image', validators=[
@@ -35,12 +35,27 @@ class EventForm(FlaskForm):
                 FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, JPEG, png, jpg, jpeg')])
     price = IntegerField('Price($)', validators=[InputRequired()])
     location = StringField( 'Location', validators=[InputRequired()])
-    datetime = DateTimeField("Date", format= '%Y-%m-%d', validators=[InputRequired()])
-    capacity = IntegerField("Capacity", validators=[InputRequired(), NumberRange(min=50, max=1000)])
+    datetime = DateTimeField('Event Date (DD/MM/YYYY hh:mm AM/PM)', validators=[InputRequired()], format='%d/%m/%Y %I:%M %p')
+    capacity = IntegerField("Capacity", validators=[InputRequired(), NumberRange(min=1, message="Capacity should be greater than 0")])
     category = StringField('Category', validators=[InputRequired()])
     submit = SubmitField("Create")
     
-#User cancel event
+#Update Event
+class UpdateEventForm(FlaskForm):
+    name = StringField('Title of the Event', validators=[InputRequired()])
+    description = TextAreaField('Description', 
+                validators=[InputRequired()])
+    image = FileField('Image', validators=[
+                FileRequired(message = 'Image cannot be empty'),
+                FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, JPEG, png, jpg, jpeg')])
+    price = IntegerField('Price($)', validators=[InputRequired()])
+    location = StringField( 'Location', validators=[InputRequired()])
+    datetime = DateTimeField('Event Date (DD/MM/YYYY hh:mm AM/PM)', validators=[InputRequired()], format='%d/%m/%Y %I:%M %p')
+    capacity = IntegerField("Capacity", validators=[InputRequired(), NumberRange(min=1, message="Capacity should be greater than 0")])
+    category = StringField('Category', validators=[InputRequired()])
+    submit = SubmitField("Update Event")
+    
+#Cancel event
 class CancelEvent(FlaskForm):
     submit = SubmitField('Cancel Event')
 
