27c793de4e23edefd7cf5348a3757568b5e24f4e
Justin Lee <smsc19970205@gmail.com>
2024-10-28 20:32:09 +1000

    Update forms, and views

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+#Need to connect database in here.
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
+    #Use database connection to return results which user want to see.
+    #e.g., if user select rock category, then return events for rock categoty from database.
     return '<h1>Starter code for assignment 3<h1>'
