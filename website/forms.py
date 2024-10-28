1ef6c9de6693078ec36095565079d5e747f86008
Justin Lee <smsc19970205@gmail.com>
2024-10-28 22:15:49 +1000

    Update form.py

--- website/forms.py
+++ website/forms.py
@@ -45,9 +45,6 @@ class UpdateEventForm(FlaskForm):
     name = StringField('Title of the Event', validators=[InputRequired()])
     description = TextAreaField('Description', 
                 validators=[InputRequired()])
-    image = FileField('Image', validators=[
-                FileRequired(message = 'Image cannot be empty'),
-                FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, JPEG, png, jpg, jpeg')])
     price = IntegerField('Price($)', validators=[InputRequired()])
     location = StringField( 'Location', validators=[InputRequired()])
     datetime = DateTimeField('Event Date (DD/MM/YYYY hh:mm AM/PM)', validators=[InputRequired()], format='%d/%m/%Y %I:%M %p')
