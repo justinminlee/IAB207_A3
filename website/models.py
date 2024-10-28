f89e7e454225ee45e72539dd34c038712aa305a9
Justin Lee <smsc19970205@gmail.com>
2024-10-28 20:33:55 +1000

    Comments in models.py

--- website/models.py
+++ website/models.py
@@ -2,6 +2,9 @@ from . import db
 from datetime import datetime
 from flask_login import UserMixin
 
+#Here we need to create Tables for the database.
+#This will needs to be done before we run the application.
+
 class User(db.Model, UserMixin):
 
 class Event(db.Model):
