f6d120954d19c787105c763bc756050be1bdcadd
Justin Lee <smsc19970205@gmail.com>
2024-10-29 23:21:28 +1000

    __init__.py done

--- website/__init__.py
+++ website/__init__.py
@@ -50,10 +50,10 @@ def create_app():
     
     @app.errorhandler(404)
     def not_found_error(error):
-       return render_template('404.html'), 404
+       return render_template('error.html', error_code = 400), 404
     
     @app.errorhandler(500)
     def internal_error(error):
-       return render_template('500.html'), 500
+       return render_template('error.html', error_code = 500), 500
     
     return app
