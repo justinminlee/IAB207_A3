d7c44609c3cc7683faae4ca19b627ae0e9e53105
Justin Lee <smsc19970205@gmail.com>
2024-10-29 23:19:07 +1000

    Update error.html

--- website/__init__.py
+++ website/__init__.py
@@ -50,7 +50,7 @@ def create_app():
     
     @app.errorhandler(404)
     def not_found_error(error):
-       return render_template('404.html'), 404s
+       return render_template('404.html'), 404
     
     @app.errorhandler(500)
     def internal_error(error):
