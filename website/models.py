625ac8813c944b94c3d07c0b7d6a1e87d70210b6
Sharpoodle <kalifll@icloud.com>
2024-10-29 15:43:59 +1000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -17,9 +17,27 @@ class Event(db.Model):
     __tablename__ = 'event'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    description = db.c
-#Here we need to create Tables for the database.
-#This will needs to be done before we run the application.
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(400))
+    ticketprice = db.Column(db.String(80))
+    #Creates the comments 
+    comments = db.relationship('Comment', backref = 'destination')
+
+    def __rep__(self):
+        return f"Name: {self.name}"
+    
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(400))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    # add the foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+
+    # string print method
+    def __repr__(self):
+        return f"Comment: {self.text}"
 
 #class User(db.Model, UserMixin):
 
