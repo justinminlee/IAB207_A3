90815802bdb6ae2bfa70f8cce989f8a945c075f6
Vivid809 <mutahhar.you@gmail.com>
2024-10-29 15:59:44 +1000

    Changes were for forms, models and event to match, will make further changes

--- website/forms.py
+++ website/forms.py
@@ -1,62 +1,61 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, IntegerField, DateTimeField, FileField
+from wtforms import TextAreaField, SubmitField, StringField, PasswordField, IntegerField, DateTimeField, FileField, SelectField
 from wtforms.validators import InputRequired, Length, Email, EqualTo, NumberRange
-from flask_wtf.file import FileRequired, FileField, FileAllowed
+from flask_wtf.file import FileRequired, FileAllowed
 
 ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
 
-# creates the login information
+# Login form
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    user_name = StringField("User Name", validators=[InputRequired('Enter user name')])
+    password = PasswordField("Password", validators=[InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
- # this is the registration form
+# Registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    first_name=StringField("First Name", validators=[InputRequired()])
-    last_name=StringField("Last Name", validators=[InputRequired()]) 
+    user_name = StringField("User Name", validators=[InputRequired()])
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()]) 
     email_id = StringField("Email Address", validators=[Email("Please enter a valid email")]) 
-    # linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
+    password = PasswordField("Password", validators=[InputRequired(), EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
-
-    # submit button
     submit = SubmitField("Register")
-    
-#Create new event
+
+# Create Event form
 class EventForm(FlaskForm):
     name = StringField('Title of the Event', validators=[InputRequired()])
-    description = TextAreaField('Description', 
-                validators=[InputRequired()])
+    description = TextAreaField('Description', validators=[InputRequired(), Length(max=500)])
     image = FileField('Image', validators=[
-                FileRequired(message = 'Image cannot be empty'),
-                FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, JPEG, png, jpg, jpeg')])
-    price = IntegerField('Price($)', validators=[InputRequired()])
-    location = StringField( 'Location', validators=[InputRequired()])
+        FileRequired(message='Image cannot be empty'),
+        FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, JPEG')])
+    price = IntegerField('Price($)', validators=[InputRequired(), NumberRange(min=1, message="Price must be greater than 0")])
+    location = StringField('Location', validators=[InputRequired()])
     datetime = DateTimeField('Event Date (DD/MM/YYYY hh:mm AM/PM)', validators=[InputRequired()], format='%d/%m/%Y %I:%M %p')
-    capacity = IntegerField("Capacity", validators=[InputRequired(), NumberRange(min=1, message="Capacity should be greater than 0")])
+    capacity = IntegerField("Capacity", validators=[InputRequired(), NumberRange(min=1, message="Capacity must be greater than 0")])
     category = StringField('Category', validators=[InputRequired()])
     submit = SubmitField("Create")
-    
-#Update Event
+
+# Update Event form
 class UpdateEventForm(FlaskForm):
     name = StringField('Title of the Event', validators=[InputRequired()])
-    description = TextAreaField('Description', 
-                validators=[InputRequired()])
-    price = IntegerField('Price($)', validators=[InputRequired()])
-    location = StringField( 'Location', validators=[InputRequired()])
+    description = TextAreaField('Description', validators=[InputRequired(), Length(max=500)])
+    price = IntegerField('Price($)', validators=[InputRequired(), NumberRange(min=1, message="Price must be greater than 0")])
+    location = StringField('Location', validators=[InputRequired()])
     datetime = DateTimeField('Event Date (DD/MM/YYYY hh:mm AM/PM)', validators=[InputRequired()], format='%d/%m/%Y %I:%M %p')
-    capacity = IntegerField("Capacity", validators=[InputRequired(), NumberRange(min=1, message="Capacity should be greater than 0")])
+    capacity = IntegerField("Capacity", validators=[InputRequired(), NumberRange(min=1, message="Capacity must be greater than 0")])
     category = StringField('Category', validators=[InputRequired()])
     submit = SubmitField("Update Event")
-    
-#Cancel event
+
+# Cancel Event form
 class CancelEvent(FlaskForm):
     submit = SubmitField('Cancel Event')
 
-#User comment
+# Comment form
 class CommentForm(FlaskForm):
-    text = TextAreaField('Leave Comment', [InputRequired()])
-    submit = SubmitField('Create')
+    text = TextAreaField('Leave Comment', validators=[InputRequired(), Length(max=300)])
+    submit = SubmitField('Post Comment')
+
+# Booking form to handle ticket quantity
+class BookingForm(FlaskForm):
+    quantity = IntegerField('Ticket Quantity', validators=[InputRequired(), NumberRange(min=1, message="Quantity must be at least 1")])
+    submit = SubmitField('Book Now')
